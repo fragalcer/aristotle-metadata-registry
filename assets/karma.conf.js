f5304356f84ea0ad950a2d490c4ad6d17b2afd4e
Francisco Gallardo <fragalcer@gmail.com>
2019-11-18 15:18:09 +1100

    Testing.

--- assets/karma.conf.js
+++ assets/karma.conf.js
@@ -11,6 +11,7 @@ dev_wp_config['optimization'] = {minimize: false}
 process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = (config) => {
+    // test
     config.set({
         frameworks: ['mocha'],
         browsers: ['ChromeHeadlessNoSandbox'],
